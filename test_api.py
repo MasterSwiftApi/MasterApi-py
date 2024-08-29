121dc4dd429e803de0dc8b498ca717520508fd6d
Nikhesh <nikhesh.p@kambala.co.in>
2024-08-29 17:51:39 +0530

    access_type added

--- test_api.py
+++ test_api.py
@@ -14,9 +14,10 @@ factor2 = <2nd factor>
 vc      = <vendor code>
 app_key = <secret key>
 imei    = <imei>
+access_type="API2"
 
 #make the api call
-ret = api.login(userid=uid, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei)
+ret = api.login(userid=user, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei,access_type=access_type)
 
 print(ret)
 
